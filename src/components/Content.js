a3e9bf9b646fba58ba5cf7f807016e310c4ab395
Follkner <follkner1@gmail.com>
2018-04-02 01:31:10 +0400

    Add ability to calculate currencies

--- src/components/Content.js
+++ src/components/Content.js
@@ -6,9 +6,7 @@ class Content extends Component {
 
 	constructor() {
 	    super();
-	    this.state = {
-	    	index : null,
-	    };		
+		
     }
 
 	componentWillMount(){
@@ -30,22 +28,77 @@ class Content extends Component {
 	}
 	
 	funcContentText(){
-		if (dataAPI[1] === undefined) return ''
+		if (dataAPI[0] === undefined) return ''
 			else return dataAPI[this.props.cont]
 	}
-
+	
 	render() {
 
+		let funcOnChangeBuy = () => {
+			let elemInput = document.getElementById('inputBuy');
+			let elemOutput = document.getElementById('outputBuy');
+			let result;
+			try{
+				result = (Math.round(dataAPI[this.props.cont].buy * elemInput.value*100))/100;
+
+				elemOutput.innerHTML = result;
+
+				if(isNaN(result) || result < 0){
+					elemOutput.innerHTML = 'Please, enter a positive numeric value';
+				}
+			}
+			catch(e){
+				console.log('undef')
+			}
+		}
+
+		let funcOnChangeSale = () => {
+			let elemInput = document.getElementById('inputSale');
+			let elemOutput = document.getElementById('outputSale');
+			let result;
+			try{
+				result = (Math.round(dataAPI[this.props.cont].sale * elemInput.value*100))/100;
+				elemOutput.innerHTML = result;
+
+				if(isNaN(result) || result < 0){
+					elemOutput.innerHTML = 'Please, enter a positive numeric value';
+				}
+			}
+			catch(e){
+				
+			}
+		}
+			
+		let nowDate = new Date();
+		let valDate = nowDate.getDate() + '.' + (nowDate.getMonth()+1)+ '.' + nowDate.getFullYear();
+		
 		return (
 			<div className = 'content'>
-			
-				<h2>{'Buy: '}</h2>
-				
-				{this.funcContentText().buy}
 
-				<h2>{'Sale: '}</h2>
-				{this.funcContentText().sale}
-			
+			{funcOnChangeBuy()}
+			{funcOnChangeSale()}
+				<div className = 'block'>
+					<div>BUY: <br></br>{this.funcContentText().buy}</div>
+					<input id = 'inputBuy' type = 'text' placeholder = 'Enter the amount for buy' 
+						   onChange = {funcOnChangeBuy} >
+					</input>
+
+					<div id = 'outputBuy'>
+					</div>
+				</div>
+
+				<div className = 'block'>
+					<div>SALE: <br></br>{this.funcContentText().sale}</div>
+					<input id = 'inputSale' type = 'text' placeholder = 'Enter the amount for sale' 
+						   onChange = {funcOnChangeSale}>
+					</input>
+					<div id = 'outputSale'>
+					</div>
+				</div>
+
+				<footer>
+					Exchange rate as of {valDate}
+				</footer>
 			</div>
 		)
 	}
